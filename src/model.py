cd68a28c6c70e2cbaadfcede7b8a42313284133f
Sebastián Dolgonos <bass.dolg@gmail.com>
2022-11-19 15:56:14 +0100

    set up training loop

--- src/model.py
+++ src/model.py
@@ -10,23 +10,22 @@ import torch.optim as optim
 
 class Net(nn.Module):
 	
-    def __init__(self):
+    def __init__(self, input_shape, classes):
         super(Net, self).__init__()
-        
         self.conv = nn.Sequential(
-                nn.Conv2d(3, 6, 5),
+                nn.Conv2d(input_shape[0], 6, 5),
                 nn.ReLU(),
                 nn.MaxPool2d(2,2), 
                 nn.Conv2d(6, 16, 5),
                 nn.ReLU(),
                 nn.MaxPool2d(2,2))
-        conv_out_size = self._get_conv_out(3)
+        conv_out_size = self._get_conv_out(input_shape)
         self.fc = nn.Sequential(
             nn.Linear(conv_out_size, 120),
             nn.ReLU(),
             nn.Linear(120, 84), 
             nn.ReLU(),
-            nn.Linear(84,3))
+            nn.Linear(84, classes))
 
     def forward(self, x):
            conv_out = self.conv(x).view(x.size()[0], -1)
